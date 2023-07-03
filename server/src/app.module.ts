aded0b8e811d75da0896ede7a5b4c700f20866dd
Igor Polischuk <polischuk01@gmail.com>
2023-07-03 17:56:00 +0300

    connected db with TypeOrm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,33 @@
+/* eslint-disable prettier/prettier */
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { join } from 'path';
+
+import { UserModule } from './user/user.module';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    }),
+    UserModule,
+    PetsModule,
+  ],
 })
 export class AppModule {}
