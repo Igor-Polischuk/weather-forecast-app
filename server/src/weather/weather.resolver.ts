4c2721383eda1c3befd10b2d8d59dceea50dc5fe
Igor Polischuk <polischuk01@gmail.com>
2023-07-27 12:41:52 +0300

    removed getCurrentWeatherInUserCities method

--- server/src/weather/weather.resolver.ts
+++ server/src/weather/weather.resolver.ts
@@ -2,13 +2,10 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 
 import { CurrentWeatherOutput } from './dto/output/current-weather.output';
-import { CurrentUser } from 'src/users/decorators/CurrentUser';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { ForecastOutput } from './dto/output/forecast.output';
 import { WeatherInput } from './dto/input/weather.input';
 import { WeatherService } from './weather.service';
-import { IUser } from 'src/users/dto/User';
-import { CitiesCurrentWeatherOutput } from './dto/output/cities-current-weather';
 
 @Resolver()
 @UseGuards(JwtAuthGuard)
@@ -29,10 +26,5 @@ export class WeatherResolver {
       weatherInput.city,
       weatherInput.units,
     );
-  }
-
-  @Query(() => [CitiesCurrentWeatherOutput])
-  getCurrentWeatherInUserCities(@CurrentUser() user: IUser) {
-    return this.weatherService.getCurrentWeatherInUserCities(user);
   }
 }
--- server/src/weather/weather.service.ts
+++ server/src/weather/weather.service.ts
@@ -2,10 +2,8 @@ import { UsersService } from 'src/users/users.service';
 import { Injectable } from '@nestjs/common';
 
 import { WeatherApiService } from 'src/external-api/weather-api/weather-api.service';
-import { CitiesCurrentWeatherOutput } from './dto/output/cities-current-weather';
 import { CurrentWeatherOutput } from './dto/output/current-weather.output';
 import { CityService } from './../city/city.service';
-import { IUser } from 'src/users/dto/User';
 
 @Injectable()
 export class WeatherService {
@@ -26,27 +24,5 @@ export class WeatherService {
   async getForecast(city: string, unit: string) {
     const coordinate = await this.cityService.getCityCoordinate(city);
     return this.weatherApiService.getForecast(coordinate, unit);
-  }
-
-  async getCurrentWeatherInUserCities(
-    user: IUser,
-  ): Promise<CitiesCurrentWeatherOutput[]> {
-    const userInDb = await this.usersService.findOne(user.id);
-
-    const weatherPromises = userInDb.cities.map(async (city) => {
-      const weather = await this.weatherApiService.getCurrentWeather(
-        {
-          lat: city.lat,
-          lon: city.lon,
-        },
-        'metric',
-      );
-
-      return { ...weather, city: city };
-    });
-
-    const weatherList = await Promise.all(weatherPromises);
-
-    return weatherList;
   }
 }
