2ddebcba77d259368a5f66d87dedf8b0cf81b505
Igor Polischuk <polischuk01@gmail.com>
2023-08-03 16:13:40 +0300

    implemented saving and removing cities by user

---
+++ client/src/modules/weather/helpers/update-cities-weather-cache.ts
@@ -0,0 +1,50 @@
+import { ApolloCache } from "@apollo/client";
+import { WeatherInUserCitiesQuery, GetCurrentWeatherQuery } from "@/gql";
+import { CURRENT_WEATHER } from "../graphql/query/getCurrentWeather";
+
+interface IUpdateCacheParams {
+  cache: ApolloCache<WeatherInUserCitiesQuery>;
+  city: string;
+  action: 'ADD' | 'REMOVE';
+}
+
+export function updateCitiesWeatherCache({ cache, city, action }: IUpdateCacheParams) {
+  cache.modify({
+    fields: {
+      getCurrentWeatherInUserCities(existingCitiesData: WeatherInUserCitiesQuery['getCurrentWeatherInUserCities']) {
+        if (action === 'ADD') {
+          const weatherInSavedCity = cache.readQuery({
+            query: CURRENT_WEATHER,
+            variables: { cityName: city },
+          }) as GetCurrentWeatherQuery;
+
+          console.log(weatherInSavedCity.currentWeather.weather);
+
+          const newItem = {
+            __typename: "UserCitiesCurrentWeather",
+            city: {
+              __typename: "City",
+              fullname: city,
+            },
+            weatherInCity: {
+              __typename: "CurrentWeatherOutput",
+              weather: weatherInSavedCity.currentWeather.weather,
+            },
+          };
+
+          return {
+            ...existingCitiesData,
+            list: [...existingCitiesData.list, newItem],
+          };
+        } else if (action === 'REMOVE') {
+          return {
+            ...existingCitiesData,
+            list: existingCitiesData.list.filter((cityData) => cityData.city.fullname !== city),
+          };
+        }
+
+        return existingCitiesData;
+      },
+    },
+  });
+}
--- client/src/modules/weather/hooks/useCityManagement.ts
+++ client/src/modules/weather/hooks/useCityManagement.ts
@@ -1,61 +1,46 @@
+import { useRemoveCityMutation, useSaveCityMutation } from "@/gql";
+import { useReactiveVar } from "@apollo/client";
 import { currentCityVar } from "@/apollo/weather-vars";
-import { useUserCitiesQuery, useRemoveCityMutation, useSaveCityMutation, RemoveCityMutation } from "@/gql";
-import { USER_CITIES } from "@/modules/user/graphql/query/UserCities";
-import { ApolloCache, FetchResult, useReactiveVar } from "@apollo/client";
-
-const updateCityCache = (cache: ApolloCache<unknown>,
-    result: Omit<FetchResult<RemoveCityMutation>, "context">) => {
-    cache.writeQuery({
-        query: USER_CITIES,
-        data: {
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            cities: [...result.data!.cities],
-        },
-    });
-};
+import { useSavedCityWeather } from "./useSavedCityWeather";
+import { updateCitiesWeatherCache } from "../helpers/update-cities-weather-cache";
 
 export function useCityManagement() {
-    const { data } = useUserCitiesQuery();
-    const currentCity = useReactiveVar(currentCityVar);
+  const { weatherInfo, total } = useSavedCityWeather();
+  const currentCity = useReactiveVar(currentCityVar);
 
-    const [removeCity, { loading: removeLoading }] = useRemoveCityMutation({
-        update: (cache, result) => updateCityCache(cache, result),
+  const [removeCity, { loading: removeLoading }] = useRemoveCityMutation();
+  const [saveCity, { loading: saveLoading }] = useSaveCityMutation();
+
+  const loading = removeLoading || saveLoading;
+
+  const cityLimit = Number(import.meta.env.VITE_CITY_LIMIT);
+  const cityName = currentCity.split(",")[0];
+
+  const isSavedCity = !!weatherInfo.find((info) => info.city === currentCity);
+
+  const onRemove = () => {
+    removeCity({
+      variables: { cityName: currentCity },
+      update: (cache) => updateCitiesWeatherCache({ cache, action: 'REMOVE', city: currentCity }),
     });
+  };
 
-    const [saveCity, { loading: saveLoading }] = useSaveCityMutation({
-        update: (cache, result) => updateCityCache(cache, result),
+  const onSave = () => {
+    saveCity({
+      variables: { cityName: currentCity },
+      update: (cache) => updateCitiesWeatherCache({ cache, action: 'ADD', city: currentCity }),
     });
+  };
 
-    const loading = removeLoading || saveLoading;
+  const cantSaveCity = (total === cityLimit && !isSavedCity) || currentCity === '';
 
-    const cityLimit = Number(import.meta.env.VITE_CITY_LIMIT);
-    const cityName = currentCity.split(",")[0];
-
-    const isSavedCity = !!data?.cities.find(
-        (city) => city.fullname === currentCity
-    );
-
-    const onRemove = () => {
-        removeCity({
-            variables: { cityName: currentCity },
-        });
-    };
-
-    const onSave = () => {
-        saveCity({
-            variables: { cityName: currentCity },
-        });
-    };
-
-    const cantSaveCity = (data?.cities.length === cityLimit && !isSavedCity) ||
-        currentCity === ""
-
-    return {
-        onSave,
-        onRemove,
-        loading,
-        cantSaveCity,
-        isSavedCity,
-        cityName
-    }
+  return {
+    onSave,
+    onRemove,
+    loading,
+    cantSaveCity,
+    isSavedCity,
+    cityName,
+  };
 }
+
