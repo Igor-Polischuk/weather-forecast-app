5feeee3787078306c95c930bff2469212e91afba
Igor Polischuk <polischuk01@gmail.com>
2023-08-09 14:32:00 +0300

    fixed weather loading error

--- client/src/modules/weather/components/Weather/index.tsx
+++ client/src/modules/weather/components/Weather/index.tsx
@@ -3,8 +3,7 @@ import { Skeleton, Space } from "antd";
 import { CurrentWeatherMainInfo } from "@modules/weather/UI/CurrentWeatherMainInfo";
 import { AdditionalWeatherInfo } from "@modules/weather/UI/AdditionalWeatherInfo";
 import { useWeatherData } from "@modules/weather/hooks/useWeatherData";
-import { useDefaultCity } from "@modules/weather/hooks/useDefaultCity";
-import { InfoDisplay } from "@/modules/common/UI/InfoDisplay";
+import { InfoDisplay } from "@modules/common/UI/InfoDisplay";
 
 import { Forecast } from "../Forecast";
 import { CitySaveRemoveToggle } from "../CitySaveRemoveToggle";
@@ -12,50 +11,47 @@ import { CitySaveRemoveToggle } from "../CitySaveRemoveToggle";
 import styles from "./styles.module.scss";
 
 export const Weather = () => {
-  const { data, loading, error } = useWeatherData();
-  const { cityName, loading: cityLoading, isCity } = useDefaultCity();
+  const { data, loading, error, cityName } = useWeatherData();
 
-  if (loading || cityLoading) {
+  if (loading) {
     return <Skeleton active />;
   }
 
-  if (!isCity || error) {
+  if (!data?.currentWeather || error) {
     const messageText = error
       ? error.message
       : "Use the search panel to find weather in your city";
     return <InfoDisplay text={messageText} />;
   }
 
-  if (data?.currentWeather) {
-    const {
-      temperature,
-      feelsLike,
-      icon,
-      weatherDescription,
-      weatherCondition,
-      humidity,
-      pressure,
-      windSpeed,
-    } = data.currentWeather.weather;
+  const {
+    temperature,
+    feelsLike,
+    icon,
+    weatherDescription,
+    weatherCondition,
+    humidity,
+    pressure,
+    windSpeed,
+  } = data.currentWeather.weather;
 
-    return (
-      <Space direction="vertical" size="large" className={styles.wrapper}>
-        <CurrentWeatherMainInfo
-          city={cityName}
-          feelsLike={feelsLike}
-          icon={icon}
-          temperature={temperature}
-          weatherDesc={weatherDescription}
-          weatherMain={weatherCondition}
-          button={<CitySaveRemoveToggle />}
-        />
-        <AdditionalWeatherInfo
-          humidity={humidity}
-          pressure={pressure}
-          windSpeed={windSpeed}
-        />
-        <Forecast cityName={cityName} />
-      </Space>
-    );
-  }
+  return (
+    <Space direction="vertical" size="large" className={styles.wrapper}>
+      <CurrentWeatherMainInfo
+        city={cityName}
+        feelsLike={feelsLike}
+        icon={icon}
+        temperature={temperature}
+        weatherDesc={weatherDescription}
+        weatherMain={weatherCondition}
+        button={<CitySaveRemoveToggle />}
+      />
+      <AdditionalWeatherInfo
+        humidity={humidity}
+        pressure={pressure}
+        windSpeed={windSpeed}
+      />
+      <Forecast cityName={cityName} />
+    </Space>
+  );
 };
--- client/src/modules/weather/hooks/useWeatherData.ts
+++ client/src/modules/weather/hooks/useWeatherData.ts
@@ -3,24 +3,30 @@ import { useEffect } from "react";
 
 import { GetCurrentWeatherQuery, useGetCurrentWeatherLazyQuery } from "@/gql";
 import { currentCityVar } from "@/apollo/weather-vars";
+import { useSavedCityWeather } from "./useSavedCityWeather";
 
 interface IUseWeatherDataReturningType {
   data: GetCurrentWeatherQuery | undefined;
   loading: boolean;
   error: ApolloError | undefined;
+  cityName: string;
 }
 
 export function useWeatherData(): IUseWeatherDataReturningType {
+  const [getCityWeather, { data, loading: weatherLoading, error }] = useGetCurrentWeatherLazyQuery();
+  const { weatherInfo, loading: cityLoading, total } = useSavedCityWeather();
   const cityName = useReactiveVar(currentCityVar);
 
-
-  const [getCityWeather, { data, loading, error }] = useGetCurrentWeatherLazyQuery();
+  const loading = (!data?.currentWeather && total > 0) || weatherLoading || cityLoading
 
   useEffect(() => {
+    if (cityName === "" && weatherInfo.length > 0) {
+      currentCityVar(weatherInfo[0].city);
+  }
     if (cityName !== '') {
       getCityWeather({ variables: { cityName } });
     }
-  }, [cityName, getCityWeather]);
+  }, [cityName, getCityWeather, weatherInfo]);
 
-  return { data, loading, error };
+  return { data, loading, error, cityName };
 }
